3c9755c384343b53eabf9c40916cf4ee10ae4965
crmfoli <crmfoli@yahoo.com.br>
2020-10-10 19:01:28 -0300

    added heroku config

--- django_heroku/settings.py
+++ django_heroku/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v!a3pk$v&612n4mq(#1#np^baz0#$3npf6ejhy$p^#b9cv02-!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-crmfoli.herokuapp.com/","127.0.0.1"]
 
 
 # Application definition
@@ -75,10 +76,7 @@ WSGI_APPLICATION = 'django_heroku.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3")
 }
 
 
